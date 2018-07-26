2817ed46d138b3aefc85bf8143f6245eab6edc0b
Rodolpho Porto <rodolpho.porto@gmail.com>
2018-07-26 11:18:36 -0300

    alterações simples

--- deploys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh      
-ssh jenkins@178.128.73.43 <<EOF        
- cd /var/lib/jenkins/workspace/codeforgeek-electronapp  
- git pull       
- npm install --production       
- pm2 restart all
- exit
-EOF
